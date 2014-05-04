db42c5f59cd39f2970755038e8d766a95b5a240b
Omar Trinidad Gutiérrez Méndez <omar.vpa@gmail.com>
2014-05-04 15:02:41 -0500

    Generated more images, change in the distribution of images in text

--- images/createImages.m
+++ images/createImages.m
@@ -21,8 +21,84 @@
 %        imshow(lennaGray);
 %        res = strcat('-r', num2str(x));
 %        name = strcat('dpi/dpi', num2str(x), '.jpg');
-%        print( fig, name, '-djpeg', res);
+%        print(fig, name, '-djpeg', res);
 %    end
 %end
 
 % generate histograms
+%function histogramsEPS()
+%    mandril = imread('histo/mandril.tiff');
+%    mandrilGray = rgb2gray(mandril);
+%
+%    % mandril image
+%    fig = figure;
+%    imshow(mandrilGray);
+%    print(fig, '-djpeg', 'histo/mandril.jpg');
+%
+%    % histogram of the mandril image
+%    [a b] = imhist(mandrilGray);
+%    x = 0:11:2805;
+%
+%    disp(size(a));
+%    bar(x, a); grid on;
+%    set(gca,'box', 'on', 'linewidth', 2.5, 'fontsize', 16);
+%    xlabel('Rango de Intensidad', 'fontsize', 23);
+%    ylabel('Frequencia', 'fontsize', 23);
+%    ylim([0, 2800]);
+%    print(fig, '-djpeg', 'histo/histogram-mandril.jpg');
+%
+%    % mandril image after histogram equalization
+%    completeRange = adapthisteq(mandrilGray);
+%    fig = figure;
+%    imshow(completeRange);
+%    print(fig, '-djpeg', 'histo/completerange.jpg');
+%
+%    % histogram of the mandril image
+%    [a b] = imhist(completeRange);
+%    bar(a) ; grid on;
+%    set(gca,'box', 'on', 'linewidth', 2.5, 'fontsize', 16);
+%    xlabel('Rango de Intensidad', 'fontsize', 23);
+%    ylabel('Frequencia', 'fontsize', 23);
+%    ylim([0, 2800]);
+%    print(fig, '-djpeg', 'histo/histogram-completerange.jpg');
+%end
+
+% histograms and contrast
+function histogramsEPS()
+    mandril = imread('histo/mandril.tiff');
+    mandrilGray = rgb2gray(mandril);
+
+    % mandril image
+    fig = figure;
+    imshow(mandrilGray);
+    print(fig, '-djpeg', 'histo/mandril.jpg');
+
+    % histogram of the mandril image
+    [a b] = imhist(mandrilGray);
+    x = 0:11:2805;
+
+    disp(size(a));
+    bar(x, a); grid on;
+    set(gca,'box', 'on', 'linewidth', 2.5, 'fontsize', 16);
+    xlabel('Rango de Intensidad', 'fontsize', 23);
+    ylabel('Frequencia', 'fontsize', 23);
+    ylim([0, 2800]);
+    print(fig, '-djpeg', 'histo/histogram-mandril.jpg');
+
+    % mandril image after histogram equalization
+    completeRange = adapthisteq(mandrilGray);
+    fig = figure;
+    imshow(completeRange);
+    print(fig, '-djpeg', 'histo/completerange.jpg');
+
+    % histogram of the mandril image
+    [a b] = imhist(completeRange);
+    bar(a) ; grid on;
+    set(gca,'box', 'on', 'linewidth', 2.5, 'fontsize', 16);
+    xlabel('Rango de Intensidad', 'fontsize', 23);
+    ylabel('Frequencia', 'fontsize', 23);
+    ylim([0, 2800]);
+    print(fig, '-djpeg', 'histo/histogram-completerange.jpg');
+end
+
+
