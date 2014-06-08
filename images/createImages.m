405cd0d03baf1d45ecf71c1b9390b8ffce1d7ce9
Omar Trinidad Gutiérrez Méndez <omar.vpa@gmail.com>
2014-06-08 09:40:30 -0500

    First commit in tha last day of edition

--- images/createImages.m
+++ images/createImages.m
@@ -25,7 +25,7 @@
 %    end
 %end
 
-% generate histograms
+% histograms and contrast
 %function histogramsEPS()
 %    mandril = imread('histo/mandril.tiff');
 %    mandrilGray = rgb2gray(mandril);
@@ -63,42 +63,24 @@
 %    print(fig, '-djpeg', 'histo/histogram-completerange.jpg');
 %end
 
-% histograms and contrast
-function histogramsEPS()
-    mandril = imread('histo/mandril.tiff');
-    mandrilGray = rgb2gray(mandril);
-
-    % mandril image
-    fig = figure;
-    imshow(mandrilGray);
-    print(fig, '-djpeg', 'histo/mandril.jpg');
-
-    % histogram of the mandril image
-    [a b] = imhist(mandrilGray);
-    x = 0:11:2805;
-
-    disp(size(a));
-    bar(x, a); grid on;
-    set(gca,'box', 'on', 'linewidth', 2.5, 'fontsize', 16);
-    xlabel('Rango de Intensidad', 'fontsize', 23);
-    ylabel('Frequencia', 'fontsize', 23);
-    ylim([0, 2800]);
-    print(fig, '-djpeg', 'histo/histogram-mandril.jpg');
-
-    % mandril image after histogram equalization
-    completeRange = adapthisteq(mandrilGray);
-    fig = figure;
-    imshow(completeRange);
-    print(fig, '-djpeg', 'histo/completerange.jpg');
-
-    % histogram of the mandril image
-    [a b] = imhist(completeRange);
-    bar(a) ; grid on;
-    set(gca,'box', 'on', 'linewidth', 2.5, 'fontsize', 16);
-    xlabel('Rango de Intensidad', 'fontsize', 23);
-    ylabel('Frequencia', 'fontsize', 23);
-    ylim([0, 2800]);
-    print(fig, '-djpeg', 'histo/histogram-completerange.jpg');
-end
-
-
+%function medicalImages()
+%    fig = figure;
+%
+%    mr = dicomread('fnl/mr/000000.dcm');
+%    mr = f12to16bits(mr);
+%    mr = adapthisteq(mr);
+%    imshow(mr);
+%    print(fig, '-djpeg', 'medical/mr.jpg');
+%
+%    crdx = dicomread('fnl/cr-dx/000000.dcm');
+%    crdx = f12to16bits(crdx);
+%    crdx = adapthisteq(crdx);
+%    imshow(crdx);
+%    print(fig, '-djpeg', 'medical/cr-dx.jpg');
+%
+%    ct = dicomread('fnl/ct/000127.dcm');
+%    ct = f12to16bits(ct);
+%    ct = adapthisteq(ct);
+%    imshow(ct);
+%    print(fig, '-djpeg', 'medical/ct.jpg');
+%end
